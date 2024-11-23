7c2a07f581f06a7b87fe99b492cadda6bc1f1f46
Mikolaj Gregorczyk <mikolaj.gregorczyk14@gmail.com>
2024-11-23 10:28:17 +0100

    Define a blank project

---
+++ src/main/java/pl/nn/foreigncurrencyaccountservice/ForeignCurrencyAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.nn.foreigncurrencyaccountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForeignCurrencyAccountServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForeignCurrencyAccountServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/nn/foreigncurrencyaccountservice/ForeignCurrencyAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.nn.foreigncurrencyaccountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForeignCurrencyAccountServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
